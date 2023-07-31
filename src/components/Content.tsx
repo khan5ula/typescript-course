eec112dc670bddf1dcf792c6cf4145247aa06db5
Kristian Hannula <kristian.hannula@proton.me>
2023-07-31 14:31:30 +0300

    Task 9.15 done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,17 +1,18 @@
+import Part from "./Part";
+import { CoursePart } from "../types";
+
 interface ContentProps {
-  courseParts: {
-    name: string;
-    exerciseCount: number;
-  }[];
+  courseParts: CoursePart[];
 }
 
 const Content = (props: ContentProps) => {
   return (
     <div>
       {props.courseParts.map((c, index) => (
-        <p key={index}>
-          {c.name} {c.exerciseCount}
-        </p>
+        <div key={index}>
+          <Part coursePart={c} />
+          <br />
+        </div>
       ))}
     </div>
   );
